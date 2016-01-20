ac35aa786ab83d62faa9cbe0647a242ba49ca18d
NPappas <student@npappas.local>
2016-01-20 11:53:38 -0600

    Set-up Story Board and created the custom classes

---
+++ College Profile Builder/College.swift
@@ -0,0 +1,13 @@
+//
+//  College.swift
+//  College Profile Builder
+//
+//  Created by NPappas on 1/20/16.
+//  Copyright © 2016 Nick Pappas. All rights reserved.
+//
+
+import UIKit
+
+class College: NSObject {
+
+}
