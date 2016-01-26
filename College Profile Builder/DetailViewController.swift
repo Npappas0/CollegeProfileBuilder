3c5f36ac697b7bb101ea4d33044961a2c48ab747
NPappas <student@npappas.local>
2016-01-26 11:57:26 -0600

    Completed Detailed Controller and the app works!

--- College Profile Builder/DetailViewController.swift
+++ College Profile Builder/DetailViewController.swift
@@ -10,9 +10,27 @@ import UIKit
 
 class DetailViewController: UIViewController
 {
-
-    override func viewDidLoad() {
+    
+    @IBOutlet weak var name: UITextField!
+    @IBOutlet weak var location: UITextField!
+    @IBOutlet weak var enrollment: UITextField!
+    @IBOutlet weak var image: UIImageView!
+    
+    var college: College!
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
+        name.text = college.name
+        location.text = college.location
+        enrollment.text = String(college.numberOfStudents)
+        image.image = college.image
     }
-
+    
+    @IBAction func onSaveTap(sender: AnyObject)
+    {
+        college.name = name.text!
+        college.location = location.text!
+        college.numberOfStudents = Int(enrollment.text!)!
+    }
 }
--- College Profile Builder/ViewController.swift
+++ College Profile Builder/ViewController.swift
@@ -19,7 +19,6 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     override func viewDidAppear(animated: Bool)
     {
         super.viewDidAppear(animated)
-        
     }
     
     override func viewDidLoad()
@@ -29,10 +28,81 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         colleges.append(College(name: "ISU", location: "Illinois", numberOfStudents: 20788, image: UIImage(named: "ISU")!))
         colleges.append(College(name: "Purdue", location: "Indiana", numberOfStudents: 38770, image: UIImage(named: "Purdue")!))
         colleges.append(College(name: "Harper", location: "Illinois", numberOfStudents: 56441, image: UIImage(named: "Harper")!))
-        
-        
     }
 
-
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return colleges.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell
+    {
+        let cell = tableView.dequeueReusableCellWithIdentifier("MyCell", forIndexPath: indexPath)
+        cell.textLabel?.text = colleges[indexPath.row].name
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath)
+    {
+        if editingStyle == .Delete
+        {
+            colleges.removeAtIndex(indexPath.row)
+            tableView.reloadData()
+        }
+    }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?)
+    {
+        let dvc = segue.destinationViewController as! DetailViewController
+        let index = tableView.indexPathForSelectedRow?.row
+        dvc.college = colleges[index!]
+    }
+    
+    
+    @IBAction func onPlusButtonTapped(sender: UIBarButtonItem)
+    {
+        let alert = UIAlertController(title: "Add College", message: nil, preferredStyle: .Alert)
+        alert.addTextFieldWithConfigurationHandler { (textField) -> Void in
+            textField.placeholder = "Add College Here"
+        }
+        let cancelAction = UIAlertAction(title: "Cancel", style: .Cancel, handler: nil)
+        alert.addAction(cancelAction)
+        
+        let addAction = UIAlertAction(title: "Add", style: .Default) { (action) -> Void in
+            let collegeTextField = alert.textFields![0] as UITextField
+            self.colleges.append(College(name: collegeTextField.text!))
+            self.tableView.reloadData()
+        }
+        alert.addAction(addAction)
+        self.presentViewController(alert, animated: true, completion: nil)
+    }
+    
+    @IBAction func onEditButtonTapped(sender: UIBarButtonItem)
+    {
+        if sender.tag == 0
+        {
+            tableView.editing = true
+            sender.tag = 1
+        }
+        else
+        {
+            tableView.editing = false
+            sender.tag = 0
+        }
+    }
+    
+    func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool
+    {
+        return true
+    }
+    
+    func tableView(tableView: UITableView, moveRowAtIndexPath sourceIndexPath: NSIndexPath, toIndexPath destinationIndexPath: NSIndexPath)
+    {
+        let college = colleges[sourceIndexPath.row]
+        colleges.removeAtIndex(sourceIndexPath.row)
+        colleges.insert(college, atIndex: destinationIndexPath.row)
+    }
 }
+
+
 
